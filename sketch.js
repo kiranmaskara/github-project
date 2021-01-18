874d4cde2d1b7172280e41926b670d7f35a2ebc4
kiranmaskara <76471075+kiranmaskara@users.noreply.github.com>
2021-01-17 21:19:01 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var movingrect,fixedrect;
+function setup() {
+  createCanvas(1200,800);
+ fixedrect= createSprite(400, 200, 50, 50);
+movingrect=createSprite(200,200,50,50);
+fixedrect.shapeColor="blue";
+movingrect.shapeColor="blue";
+}
+
+function draw() {
+
+  background(0);  
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+  if(movingrect.x-fixedrect.x<movingrect.width/2+fixedrect.width/2
+   &&fixedrect.x-movingrect.x<movingrect.width/2+fixedrect.width/2
+   &&movingrect.y-fixedrect.y<movingrect.height/2+fixedrect.height/2
+   && fixedrect.y-movingrect.y<movingrect.height/2+fixedrect.height/2 )
+{
+  fixedrect.shapeColor="red";
+  movingrect.shapeColor="red"; 
+}
+else
+{
+  fixedrect.shapeColor="blue";
+movingrect.shapeColor="blue";
+}
+  drawSprites();
+}
